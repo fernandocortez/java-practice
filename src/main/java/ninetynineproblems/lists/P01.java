eba48c35851d151fa87b3c2794b96deccdae60d0
Fernando Cortez <fernando_c_cortez@homedepot.com>
2016-06-09 16:36:15 -0400

    First problem from ninety-nine problems

---
+++ src/main/java/ninetynineproblems/lists/P01.java
@@ -0,0 +1,16 @@
+package ninetynineproblems.lists;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class P01 {
+  public static String last(List<String> list) {
+    if (list == null || list.isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    if (list.size() == 1) {
+      return list.get(0);
+    }
+    return last(list.subList(1, list.size()));
+  }
+}
---
+++ src/test/java/ninetynineproblems/lists/P01Test.java
@@ -0,0 +1,15 @@
+package ninetynineproblems.lists;
+
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class P01Test {
+  @Test
+  public void shouldFindLastElementFromAListOfAlphabets() throws Exception {
+    assertThat(P01.last(asList("a", "b", "c", "d")), is(equalTo("d")));
+  }
+}
