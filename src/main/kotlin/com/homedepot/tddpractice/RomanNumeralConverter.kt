aeb8eb0a2668d8ef19a5ed3e0048ae26d2f52400
Fernando Cortez <fccortez1@hotmail.com>
2017-03-21 15:17:14 -0400

    Increased cases up to 4000 (exclusive)

--- src/main/kotlin/com/homedepot/tddpractice/RomanNumeralConverter.kt
+++ src/main/kotlin/com/homedepot/tddpractice/RomanNumeralConverter.kt
@@ -12,6 +12,13 @@ class RomanNumeralConverter {
       in 40..49 -> "XL" + convertIntegerToRomanNumeral(i - 40)
       in 50..89 -> "L" + convertIntegerToRomanNumeral(i - 50)
       in 90..99 -> "XC" + convertIntegerToRomanNumeral(i - 90)
+      in 100..189,
+      in 200..399 -> "C" + convertIntegerToRomanNumeral(i - 100)
+      in 190..199 -> "CX" + convertIntegerToRomanNumeral(i - 90)
+      in 400..499 -> "CD" + convertIntegerToRomanNumeral(i - 400)
+      in 500..899 -> "D" + convertIntegerToRomanNumeral(i - 500)
+      in 900..999 -> "CM" + convertIntegerToRomanNumeral(i - 900)
+      in 1000..3999 -> "M" + convertIntegerToRomanNumeral(i - 1000)
       else -> "this number is not being handled"
     }
   }
