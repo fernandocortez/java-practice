d9c91de6ad2f8209eedf5ec017b23fd95cf3531f
Fernando Cortez <fernando_c_cortez@homedepot.com>
2017-10-12 13:40:49 -0400

    Wrote more Spock tests

--- src/test/groovy/com/codingbat/warmup1/DiffTwentyOneTestGroovy.groovy
+++ src/test/groovy/com/codingbat/warmup1/DiffTwentyOneTestGroovy.groovy
@@ -14,34 +14,12 @@ class DiffTwentyOneTestGroovy extends Specification {
     result == 2
   }
 
-  def "should equal 2 when diffing with 19 using Kotlin port"() {
-    given: "input is 19"
-    def input = 19
-
-    when: "result is computed"
-    def result = DiffTwentyOneKt.diff21(input)
-
-    then: "there should be a difference of 2"
-    result == 2
-  }
-
   def "should equal 11 when diffing with 10"() {
     given: "input is 10"
     def input = 10
 
     when: "result is computed"
     def result = DiffTwentyOne.diff21(input)
-
-    then: "there should be a difference of 11"
-    result == 11
-  }
-
-  def "should equal 11 when diffing with 10 using Kotlin port"() {
-    given: "input is 10"
-    def input = 10
-
-    when: "result is computed"
-    def result = DiffTwentyOneKt.diff21(input)
 
     then: "there should be a difference of 11"
     result == 11
@@ -58,6 +36,72 @@ class DiffTwentyOneTestGroovy extends Specification {
     result == 21
   }
 
+  def "should equal 2 when diffing with 22"() {
+    given: "input is 22"
+    def input = 22
+
+    when: "result is computed"
+    def result = DiffTwentyOne.diff21(input)
+
+    then: "there should be a difference of 2"
+    result == 2
+  }
+
+  def "should equal 8 when diffing with 25"() {
+    given: "input is 25"
+    def input = 25
+
+    when: "result is computed"
+    def result = DiffTwentyOne.diff21(input)
+
+    then: "there should be a difference of 8"
+    result == 8
+  }
+
+  def "should equal 18 when diffing with 30"() {
+    given: "input is 30"
+    def input = 30
+
+    when: "result is computed"
+    def result = DiffTwentyOne.diff21(input)
+
+    then: "there should be a difference of 18"
+    result == 18
+  }
+
+  def "should equal 21 when diffing with 0"() {
+    given: "input is 0"
+    def input = 0
+
+    when: "result is computed"
+    def result = DiffTwentyOne.diff21(input)
+
+    then: "there should be a difference of 21"
+    result == 21
+  }
+
+  def "should equal 2 when diffing with 19 using Kotlin port"() {
+    given: "input is 19"
+    def input = 19
+
+    when: "result is computed"
+    def result = DiffTwentyOneKt.diff21(input)
+
+    then: "there should be a difference of 2"
+    result == 2
+  }
+
+  def "should equal 11 when diffing with 10 using Kotlin port"() {
+    given: "input is 10"
+    def input = 10
+
+    when: "result is computed"
+    def result = DiffTwentyOneKt.diff21(input)
+
+    then: "there should be a difference of 11"
+    result == 11
+  }
+
   def "should equal 0 when diffing with 21 using Kotlin port"() {
     given: "input is 0"
     def input = 0
@@ -66,6 +110,50 @@ class DiffTwentyOneTestGroovy extends Specification {
     def result = DiffTwentyOneKt.diff21(input)
 
     then: "there should be a difference of 11"
+    result == 21
+  }
+
+  def "should equal 2 when diffing with 22 using Kotlin port"() {
+    given: "input is 22"
+    def input = 22
+
+    when: "result is computed"
+    def result = DiffTwentyOneKt.diff21(input)
+
+    then: "there should be a difference of 2"
+    result == 2
+  }
+
+  def "should equal 8 when diffing with 25 using Kotlin port"() {
+    given: "input is 25"
+    def input = 25
+
+    when: "result is computed"
+    def result = DiffTwentyOneKt.diff21(input)
+
+    then: "there should be a difference of 8"
+    result == 8
+  }
+
+  def "should equal 18 when diffing with 30 using Kotlin port"() {
+    given: "input is 30"
+    def input = 30
+
+    when: "result is computed"
+    def result = DiffTwentyOneKt.diff21(input)
+
+    then: "there should be a difference of 18"
+    result == 18
+  }
+
+  def "should equal 21 when diffing with 0 using Kotlin port"() {
+    given: "input is 0"
+    def input = 0
+
+    when: "result is computed"
+    def result = DiffTwentyOneKt.diff21(input)
+
+    then: "there should be a difference of 21"
     result == 21
   }
 }
